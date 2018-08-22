1f36f7da40683d191a281d7d000dd0090591e245
Yusupov Umyar <uiusupov@bellintegrator.com>
2018-08-22 11:58:56 +0300

    обновил README

--- src/actions/photosAction.js
+++ src/actions/photosAction.js
@@ -1,6 +1,5 @@
 import {LOAD_PHOTOS_START, LOAD_PHOTOS_SUCCESS, LOAD_PHOTOS_FAIL,
         LOAD_NEXT_PHOTOS_START, LOAD_NEXT_PHOTOS_SUCCESS, LOAD_NEXT_PHOTOS_FAIL} from "./constants"
-//import axios from 'axios'
 const jsonp = require('jsonp');
 
 const access_token = 'd7f89abd338884978f0d45a661004c17529f7d799e0ab0ea772026714b6579add80952989353e934efac5';
@@ -21,26 +20,6 @@ export function getPhotos(howMany) {
                     dispatch({ type: LOAD_PHOTOS_FAIL, payload: data.error.error_msg });
                 }
             })
-
-       /* axios({
-            method: 'get',
-            url: `https://api.vk.com/method/newsfeed.get?filters=wall_photo&count=${howMany}&end_time=1516838400&access_token=${access_token}&v=5.80`,
-            dataType: 'JSONP'
-        })
-            .then(function (response) {
-                if (response.data.response) {
-                    console.log(response.data.response);
-                    dispatch({ type: LOAD_PHOTOS_SUCCESS, payload: response.data.response });
-                }
-
-                else if (response.data.error.error_code) {
-                    console.log(response.data.error.error_code);
-                    dispatch({ type: LOAD_PHOTOS_FAIL, payload: response.data.error.error_msg });
-                }
-            })
-            .catch(function () {
-                dispatch({ type: LOAD_PHOTOS_FAIL, payload: 'Сервер в данный момент не отвечает' });
-            })*/
     }
 }
 
@@ -59,23 +38,5 @@ export function getNextPhotos(next_from) {
                     dispatch({ type: LOAD_NEXT_PHOTOS_FAIL, payload: data.error.error_msg });
                 }
             })
-
-        /*axios({
-            method: 'get',
-            url: `https://api.vk.com/method/newsfeed.get?filters=wall_photo&count=1&end_time=1516838400&start_from=${next_from}&access_token=${access_token}&v=5.80`,
-            dataType: 'JSONP'
-        })
-            .then(function (response) {
-                if (response.status === 200) {
-                    dispatch({ type: LOAD_NEXT_PHOTOS_SUCCESS, payload: response.data.response });
-                }
-
-                else {
-                    dispatch({ type: LOAD_NEXT_PHOTOS_FAIL, payload: 'Не удалось загрузить фото' });
-                }
-            })
-            .catch(function () {
-                dispatch({ type: LOAD_NEXT_PHOTOS_FAIL, payload: 'Сервер в данный момент недоступен' });
-            })*/
     }
 }
